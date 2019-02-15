09d70759d7a0a2767433edd1c30dfbcc415358ea
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2019-02-15 14:20:13 -0700

    Created Assignment 9

--- Assignment-9/Tests/3.R
+++ Assignment-9/Tests/3.R
@@ -1,4 +1,12 @@
+out_file = "3.tsv"
+
+if (file.exists(out_file))
+  file.remove(out_file)
+
 suppressPackageStartupMessages(suppressWarnings(suppressMessages(source("3.R"))))
+
+if (!file.exists(out_file))
+  print(paste0("No file exists at ", out_file, "."))
 
 test_data = suppressWarnings(suppressMessages(read_tsv("3.tsv")))
 print(test_data)
